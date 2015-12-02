34ae32972048304968c70228cfd0b92688cc5d1b
Justin <justin.vanvoorst@gmail.com>
2015-12-02 16:38:32 -0700

    added recursion

--- recursion/main.js
+++ recursion/main.js
@@ -1,10 +1,28 @@
-console.log('really');
+var word = 'really';
+
+console.log(word);
+
+// function reverse(word) {
+// 	var newWord = ''; 
+// 	for (var i = word.length - 1; i >= 0; i--) {
+// 		newWord += word[i];
+// 	}
+// 	return newWord;
+// }
+// console.log(reverse(word));
+
 function reverse(word) {
-	var newWord = ''; 
-	for (var i = word.length - 1; i >= 0; i--) {
-		newWord += word[i];
-	}
-	return newWord;
-	console.log(newWord);
+	return (word === '') ? '' : reverse(word.substr(1)) + word[0];
 }
-console.log(reverse('really'));
+console.log(reverse(word));
+
+// function reverse(word) {
+// 	if (word.length > 1) {
+// 		return reverse(word.substr(1)) + word[0];
+// 	}
+// 	else {
+// 		console.log('return')
+// 		return word
+// 	}
+// }
+// console.log(reverse('old'));
