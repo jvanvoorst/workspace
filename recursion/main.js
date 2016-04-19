1c375e3eeca8881af5e7a5de8fb14067501f5de8
Justin <justin.vanvoorst@gmail.com>
2016-04-18 20:41:27 -0600

    more recursion

--- recursion/main.js
+++ recursion/main.js
@@ -5,6 +5,7 @@ console.log(word);
 function reverse(word) {
 	return (word === '') ? '' : reverse(word.substr(1)) + word[0];
 }
+
 console.log(reverse(word));
 
 function reverse(word) {
@@ -13,5 +14,14 @@ function reverse(word) {
 	}
 	return word
 }
+
 console.log(reverse(word));
+
+function double(str) {
+	var word = str;
+	return (word.length > 0) ? double(word.substring(0, word.length -1)) + (word[word.length -1] + word[word.length - 1]) : word;
+
+}
+
+console.log(double(word));
 
